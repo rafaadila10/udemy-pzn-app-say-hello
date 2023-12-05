7a8fef2c6e388e33074a3d8e5cfac82d49c536b2
Mohammad Rafa Adila <mohammadrafa63@gmail.com>
2023-12-05 11:06:38 +0700

    app say hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rafaadila10/udemy-pzn-app-say-hello
+
+go 1.21.4
+
+require github.com/rafaadila10/udemy-pzn-go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/rafaadila10/udemy-pzn-go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("Rafa"))
+}
